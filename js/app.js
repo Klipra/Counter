12f9f3fa6cdaec2b95180cd2a933196f4431b1fd
klipra <dirk78@mail,ru>
2022-10-08 15:30:02 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const items = document.querySelectorAll(".countdown-item > h4");
+const countdownElement = document.querySelector("countdown");
+//Назначаем дату отчета 
+let countdownDate = new Date( 2022, 11, 18, 10, 0, 0).getTime();
+
+
+function getCountdownTime() {
+    //Получаем текущее время
+    const now = new Date().getTime();
+
+    //Находим разницу времени
+    const distance = countdownDate - now;
+
+    //Создаем переменные в милисекунах
+    //1с = 1000мс
+    //1м = 60с
+    //1ч = 60м
+    //1д = 24ч
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    //Подсчет дней, часов, минут и секунд
+    let days =  Math.floor(distance / oneDay);
+    let hours = Math.floor(distance % oneDay) / oneHour;
+    let minutes = Math.floor(distance % oneHour) / oneMinute;
+    let seconds = Math.floor(distance % oneMinute) / 1000;
+
+    //Создаем массив с переменными
+    const values = [days, hours, minutes, seconds];
+     console.log(values);
+     
+    //Добавляем значение переменных на страницу 
+    items.forEach(function(item, index) {
+        item.textContent = (values [index]);
+    });
+    if(distance < 0) {
+        clearInterval(countdown);
+        countdownElement.innerHTML = "<h4 class='expired'>Время вышло</h4>"
+
+    }
+}
+
+let countdown = setInterval(getCountdownTime, 1000);
+
+getCountdownTime();
